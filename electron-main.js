f6429d1d0c24d8663309292064db5d9ce721bcc8
ADW11 <90983480+LukaThirden@users.noreply.github.com>
2026-05-18 16:03:08 +0200

    Prepare Brennprozess desktop app packaging

--- electron-main.js
+++ electron-main.js
@@ -13,7 +13,7 @@ function createMainWindow() {
     },
   });
 
-  win.loadFile(path.join(__dirname, 'app.html'));
+  win.loadFile(path.join(__dirname, 'brennprozess.html'));
 }
 
 app.whenReady().then(() => {
